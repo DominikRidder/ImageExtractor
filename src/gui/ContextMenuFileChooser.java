e3f59fd841f9f9ec031c8c2bbc8209065b60d475
DominikRidder <d.ridder@fz-juelich.de>
2015-10-06 16:02:06 +0200

    uploading current version, to switch from eclipse to netbeans

--- src/gui/ContextMenuFileChooser.java
+++ src/gui/ContextMenuFileChooser.java
@@ -1,16 +1,10 @@
 package gui;
 
 import java.awt.Component;
-import java.awt.Frame;
-import java.awt.HeadlessException;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
-import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
-import javax.swing.SwingUtilities;
 
 import sun.swing.FilePane;
 
@@ -29,8 +23,11 @@ public class ContextMenuFileChooser extends JFileChooser {
 		JMenuItem menuItem = new JMenuItem("Delete Folder");
 		menuItem.addActionListener(new DeleteAction(this));
 
-		FilePane output = (FilePane) this.getComponent(2);
-		output.getComponentPopupMenu().add(menuItem);
+		for (Component c : this.getComponents()){
+			if (c instanceof FilePane){
+				((FilePane)c).getComponentPopupMenu().add(menuItem);
+			}
+		}
 	}
 
 
