686d2ec900ffc49b3b37b85d99d4bc2813b97e6b
DominikRidder <d.ridder@fz-juelich.de>
2016-11-11 16:02:55 +0100

    Added multiple value option: <Keyword> = <Value1> | <Value2>

--- src/gui/ImageSearch.java
+++ src/gui/ImageSearch.java
@@ -25,54 +25,54 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-
 public class ImageSearch extends JFrame implements ActionListener, Runnable {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -6770567548026413228L;
-	
+
 	JFileChooser filechoose = new JFileChooser();
-	
+
 	JTextField searchin;
-	
+
 	JTextField filter;
-	
+
 	JTextArea outputArea;
-	
+
 	JScrollPane outputScroller;
-	
+
 	JButton startButton;
-	
+
 	JCheckBox searchAll;
-	
+
 	boolean stop = false;
-	
+
 	public ImageSearch() {
 		this.setTitle("ImageSearch");
-		
+
 		searchin = new JTextField();
 		searchin.setMaximumSize(new Dimension(Integer.MAX_VALUE, 75));
 		searchin.setMinimumSize(new Dimension(300, 75));
-		
+
 		// upperleft rectangle
 		filter = new JTextField("");
 		filter.setEditable(true);
 		filter.setMaximumSize(new Dimension(Integer.MAX_VALUE, 75));
 		filter.setMinimumSize(new Dimension(300, 75));
-		
+
 		startButton = new JButton("Start Search");
 		startButton.addActionListener(this);
-		
+
 		searchAll = new JCheckBox();
-		
-//		// creating the output area
+
+		// // creating the output area
 		outputArea = new JTextArea();
 		outputArea.setEditable(false);
 		outputArea.setMargin(new Insets(0, 0, 0, 0));
 		outputScroller = new JScrollPane(outputArea);
-		outputScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		outputScroller
+				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
@@ -85,28 +85,28 @@ public class ImageSearch extends JFrame implements ActionListener, Runnable {
 		panel.add(startButton);
 		panel.add(Box.createVerticalStrut(10));
 		panel.add(outputScroller);
-		
+
 		// adding everythis to this (class)
 		this.add(panel);
 		this.setSize(500, 400);
 		this.setVisible(true);
 	}
-	
+
 	public JPanel addDescription(JComponent textfield, String desc) {
 		JPanel row = new JPanel();
 		row.setLayout(new BoxLayout(row, BoxLayout.LINE_AXIS));
 		row.setMinimumSize(new Dimension(300, 75));
-		
+
 		JLabel label = new JLabel(desc);
-		
+
 		row.add(label);
 		row.add(textfield);
-		
+
 		return row;
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		switch(e.getActionCommand()) {
+		switch (e.getActionCommand()) {
 		case "Start Search":
 			stop = false;
 			Thread t = new Thread(this);
@@ -118,57 +118,66 @@ public class ImageSearch extends JFrame implements ActionListener, Runnable {
 			break;
 		}
 	}
-	
+
 	public void run() {
 		startButton.setText("Cancle");
 		search();
 		startButton.setText("Start Search");
 	}
-	
+
 	public void search() {
 		outputArea.setText("");
 		String dir = searchin.getText();
 		String filterStr = filter.getText();
 		HashSet<String> alreadyOutput = new HashSet<String>();
 		boolean fullsearch = searchAll.isSelected();
-		
-		for (String test : filterStr.split("&")){
+
+		for (String test : filterStr.split("&")) {
 			String parts[] = test.split("=");
-			
-			if (parts.length < 2) {
-				outputArea.setText("Invalid filter component. Please use: <Keyword> = <Value>");
-				outputArea.setText(outputArea.getText()+"\nor multiple: <Keyword> = <Value> & <Keyword> = <Value> & ...");
+
+			if (parts.length != 2) {
+				outputArea
+						.setText("Invalid filter component. Please use: <Keyword> = <Value>");
+				outputArea
+						.setText(outputArea.getText()
+								+ "\nor multiple: <Keyword> = <Value> & <Keyword> = <Value> & ...");
+				outputArea
+						.setText(outputArea.getText()
+								+ "\nor Check for diffent Values: <Keyword> = <Value> | <Value> & <Keyword> = ...");
 				return;
 			}
-			
+
 			String key = parts[0].trim();
-			String val = parts[1].trim().toLowerCase();
 			
 			if (key.equals("")) {
 				outputArea.setText("No key is given before '='");
 				return;
 			}
 			
-			if (val.equals("")) {
-				outputArea.setText("No value is given behind '='");
-				return;
+			for (String posval : parts[1].split("\\|")) {
+				String val = posval.trim().toLowerCase();
+
+				if (val.equals("")) {
+					outputArea.setText("Value error behind '='");
+					return;
+				}
 			}
 		}
-		
+
 		if (dir.equals("")) {
 			outputArea.setText("Directory not set");
 			return;
 		}
-		
+
 		Stack<File> nextFolders = new Stack<File>();
 		String path;
 		File file = new File(dir);
-		
+
 		if (!file.exists() || !file.isDirectory()) {
 			outputArea.setText("Invalid Directory");
 			return;
 		}
-		
+
 		nextFolders.push(file);
 		File[] list;
 		boolean warnedKey = false;
@@ -185,45 +194,64 @@ public class ImageSearch extends JFrame implements ActionListener, Runnable {
 				return;
 			}
 			outer: for (File potentialDicom : list) {
-				
+
 				path = potentialDicom.getAbsolutePath();
-				
+
 				if (!fullsearch && potentialDicom.getName().startsWith(".")) {
 					continue;
 				}
-				
+
 				if (!fullsearch && foundAImage) {
 					break;
-				} else if (potentialDicom.isDirectory()){
+				} else if (potentialDicom.isDirectory()) {
 					if (!Files.isSymbolicLink(potentialDicom.toPath())) {
 						nextFolders.push(potentialDicom);
 					}
-				} else if (!potentialDicom.isDirectory() && !alreadyOutput.contains(potentialDicom.getParentFile().getAbsolutePath())
+				} else if (!potentialDicom.isDirectory()
+						&& !alreadyOutput.contains(potentialDicom
+								.getParentFile().getAbsolutePath())
 						&& (path.endsWith(".dcm") || path.endsWith(".IMA") || Image
 								.isDicom(potentialDicom.toPath()))) {
 					foundAImage = true;
 					try {
 						Image img = new Image(potentialDicom.getAbsolutePath());
-						
-						for (String test : filterStr.split("&")){
+
+						for (String test : filterStr.split("&")) {
+							boolean foundOneVal = false;
 							String parts[] = test.split("=");
 							String key = parts[0].trim();
-							String val = parts[1].trim().toLowerCase();
-							
-							String extracted = img.getAttribute(key).toLowerCase().trim();
-							
-							if (!warnedKey && extracted.equals("")) {
-								outputArea.setText("WARNING: Key "+key+" is maybe not correct spelled!\n"+outputArea.getText());
-								warnedKey = true;
+							for (String posVal : parts[1].split("\\|")) {
+								String val = posVal.trim().toLowerCase();
+
+								String extracted = img.getAttribute(key)
+										.toLowerCase().trim();
+
+								if (!warnedKey && extracted.equals("")) {
+									outputArea
+											.setText("WARNING: Key "
+													+ key
+													+ " is maybe not correct spelled!\n"
+													+ outputArea.getText());
+									warnedKey = true;
+								}
+
+								if (extracted.contains(val)) {
+									foundOneVal = true;
+									break;
+								}
 							}
-							
-							if (!extracted.contains(val)) {
+
+							if (!foundOneVal) {
 								continue outer;
 							}
 						}
-						outputArea.setText(outputArea.getText()+"\n"+potentialDicom.getParentFile().getAbsolutePath());
+						outputArea.setText(outputArea.getText()
+								+ "\n"
+								+ potentialDicom.getParentFile()
+										.getAbsolutePath());
 						repaint();
-						alreadyOutput.add(potentialDicom.getParentFile().getAbsolutePath());
+						alreadyOutput.add(potentialDicom.getParentFile()
+								.getAbsolutePath());
 					} catch (Exception err) {
 						continue;
 					}
